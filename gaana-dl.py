c195003a7ef743d9bfe682f2593d6c46d7ea9ed3
TusharBabbar <babbartushar@gmail.com>
2014-08-28 20:45:44 +0530

    added gaana-dl.py

---
+++ gaana-dl.py
@@ -0,0 +1,107 @@
+import requests, re
+from bs4 import BeautifulSoup
+
+class BadHTTPCodeError(Exception):
+    def __init__(self, code):
+        print code
+
+class GaanaDownloader():
+    def __init__(self):
+        self.urls = {
+            'search' : 'http://gaana.com/search/songs/{query}',
+            'get_token' : 'http://gaana.com//streamprovider/get_stream_data_v1.php',
+            'search_album' : 'http://gaana.com/search/albums/{query}',
+            'album' : 'http://gaana.com/album/{name}'
+        }
+
+    def get_url_contents(self, url):
+        url = url.replace(' ','%20')
+        print url
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response
+        else:
+            raise BadHTTPCodeError(response.status_code)
+
+    def get_search_song(self, query, dir_name):
+        url = self.urls['search'].format(query = query)
+        response = self.get_url_contents(url)
+        soup = BeautifulSoup(response.content)
+        songs = soup.find_all('div',{'id':re.compile('_item_row_')})
+        if songs:
+            for song in songs:
+                _id = song['id'].split('_')[-1]
+                track_name = song.ul.text.strip().split('\n')[0]
+                track_name = track_name.replace(' ','-')
+                #print _id
+                if self.download_track(_id, track_name, dir_name):
+                    break;
+                #for song in songs:
+                    #track, ablum, artist = song.ul.text.strip().split('/n')[0:3]
+                    #print song.ul.text.strip().split('/n')[0:3]
+                    ###print track, album, artist
+        else:
+            print 'No song found'
+
+    def get_songs_from_album(self, name):
+        import os
+        url = self.urls['album'].format(name = name)
+        response = self.get_url_contents(url)
+        soup = BeautifulSoup(response.content)
+        songs = soup.find_all('div',{'id':re.compile('_item_row_')})
+        os.system('mkdir %s'%name)
+        if songs:
+            for song in songs:
+                _id = song['id'].split('_')[-1]
+                track_name = song.ul.text.strip().split('\n')[0]
+                track_name = track_name.replace(' ','-')
+                self.get_search_song(track_name, name)
+                #print _id
+                #if self.download_track(_id, track_name):
+                    #pass
+                ##for song in songs:
+                    #track, ablum, artist = song.ul.text.strip().split('/n')[0:3]
+                    #print song.ul.text.strip().split('/n')[0:3]
+                    ###print track, album, artist
+        else:
+            print 'No song found'
+
+
+    def get_search_album(self, query):
+        url = self.urls['search_album'].format(query = query)
+        response = self.get_url_contents(url)
+        soup = BeautifulSoup(response.content)
+        print soup
+        albums = soup.find_all('a',{'class':re.compile('play-song-small')})
+        print albums
+        if albums:
+            print 'options are :'
+            i = 0
+            for album in albums:
+                print i,album['href']
+                i +=1
+            option = raw_input('choose one number: ')
+            name = albums[int(option)]['href'].split('/')[-1]
+            print name
+            self.get_songs_from_album(name)
+
+    def download_track(self, _id, track_name, dir_name):
+        res = requests.post(self.urls['get_token'], data = {'track_id':int(_id), 'protocol':'http', 'quality': 'mp3'})
+        print res
+        if res.status_code == 200:
+            print res.json()
+            stream_path = res.json()['stream_path']
+            res = requests.get(stream_path)
+            if res.status_code == 200:
+                with open(dir_name + '/' +track_name+'.mp3', 'wb') as f:
+                    print track_name
+                    f.write(res.content)
+                return True
+            else:
+                print 'Could not download'
+
+if __name__ == '__main__':
+    import sys
+    query = sys.argv[1]
+    d = GaanaDownloader()
+    d.get_search_song(query, 'temp')
